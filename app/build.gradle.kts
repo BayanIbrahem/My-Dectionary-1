fa72f9b38969e71aafd37dd4fb1cae92ba22d3fa
Bayan <dev.bayan.ibrahim@gmail.com>
2025-02-05 20:17:13 +0300

    ship version 2.0.0-beta03

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 12
-        versionName = "2.0.0"
+        versionName = "2.0.0-beta03"
 
         multiDexEnabled = true
 
